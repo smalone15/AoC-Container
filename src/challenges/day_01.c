678c0ac2cf258531289253db6f8198b4d63bc9c9
Leo Cozzens <cozzensleo@gmail.com>
2025-12-17 14:25:08 -0600

    Changed to read binary mode, and modified incomplete read check

--- src/challenges/day_01.c
+++ src/challenges/day_01.c
@@ -1,3 +1,5 @@
+// C standard headers
+#include <stdlib.h>
 // Local headers
 #include <challenge.h>
 #include <feed.h>
@@ -5,10 +7,27 @@
 const char *dataFileName = "./data/day_01.txt";
 
 ErrorData evaluate(InputData *input, Answer *result) {
+    *result = (Answer) { "Password", 0 };
+    int position = 50;
+
     LineFeed feed = create_linefeed(input);
     for(char *line = get_linefeed(&feed); line != NULL; line = get_linefeed(&feed)) {
-        // PROCESS LINE
+        int rotations;
+        switch(*line) {
+            case 'L':
+                rotations = atoi(line + 1);
+                position -= rotations;
+                position = (position % 100 + 100) % 100;
+                break;
+            case 'R':
+                rotations = atoi(line + 1);
+                position += rotations;
+                position %= 100;
+                break;
+            default:
+                continue;
+        }
+        if(position == 0) result->output++;
     }
-    *result = (Answer) { "TestObject", 100 };
     return emptySuccess;
 }
